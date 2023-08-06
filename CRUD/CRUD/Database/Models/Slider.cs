42587c8ff222d258df86acdf28eb82ea14644334
User <User@WINDOWS-2R2MKCU>
2023-08-06 17:06:20 +0400

    The starting point is set

---
+++ CRUD/CRUD/Database/Models/Slider.cs
@@ -0,0 +1,20 @@
+﻿namespace CRUD.Database.Models;
+
+public class Slider
+{
+    public Slider(string title, string description, string image, string button, int order)
+    {
+        Title = title;
+        Description = description;
+        Image = image;
+        Button = button;
+        Order = order;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string Button { get; set; }
+    public int Order { get; set; }
+
+}
